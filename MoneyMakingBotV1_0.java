b7ea898bbda69a200bd4a701b3836f503bf77b9e
rob-roibu <roibu1@umbc.edu>
2021-04-13 20:33:03 -0400

    need to check if the best "bang for buck card is picked"

--- MoneyMakingBotV1_0.java
+++ MoneyMakingBotV1_0.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import dominionAgents.CardData.CardType;
-import jdk.internal.util.xml.impl.Pair;
+
 
 // The Bot is similar to the rush bot but more laid back, the goal of the ai is to keep buying treasure cards until they are able to buy point cards
 // priortizing higher point cards over lower value ones: in pratice the AI would buy try to get cards that allow it to buy more then once if able it buys those cards 
@@ -20,9 +20,10 @@ public class MoneyMakingBotV1_0 extends BasicBotV1_0{
       
     boolean treasureInSupply = false;
     int actionNum = 0;   
+    ArrayList<Float> ratioList = CardData.ratioCalc();
     public MoneyMakingBotV1_0(Kingdom k, PlayerCommunication pc) {
 		super(k, pc);
-        CardData.ratioCalc();
+        
 
     }
 
@@ -40,10 +41,11 @@ public class MoneyMakingBotV1_0 extends BasicBotV1_0{
            buyCard("Province");
         }
         
-        String cardToBuy = cheapestBuy();
-        if(coins < 8 && cardToBuy != null && actionNum > 5){
+        String cardToBuy = bestBuy();
+        if(coins < 8 && cardToBuy != null && actionNum < 5){
+            System.out.print("buying "+cardToBuy+"\n");
             buyCard(cardToBuy);
-            actions += 1;
+            actionNum += 1;
         }
 
 
@@ -51,10 +53,27 @@ public class MoneyMakingBotV1_0 extends BasicBotV1_0{
 
         
 	}
-    protected String cheapestBuy(){
+    protected String bestBuy(){
 
     	String s = null;
+        ArrayList<SupplyPile> supply = kingdom.getSupplyPiles();
+        Float max = (float)0;
         
+        int i = 0;
+        
+        for(SupplyPile sp: supply){
+            Card currCard = sp.getCard();
+            i = kingdom.kingdomIndex(currCard.getName());
+            if(currCard.isCardType(CardData.CardType.ACTION)){
+                
+                if(max < ratioList.get(i)){
+                    max = ratioList.get(i);
+                    System.out.print(max + "\n");
+                    s = currCard.getName();
+                }
+            }
+            
+        }
 
 
 
